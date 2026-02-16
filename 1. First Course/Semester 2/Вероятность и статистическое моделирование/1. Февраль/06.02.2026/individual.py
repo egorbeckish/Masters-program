430b52d659c26d03b7f8bf275fda18b8e3e30a80
egorbeckish <egorbeckish@mail.ru>
2026-02-16 14:20:31 +0300

    add new import and class MonteCarlo

--- 1. First Course/Semester 2/Вероятность и статистическое моделирование/1. Февраль/06.02.2026/individual.py
+++ 1. First Course/Semester 2/Вероятность и статистическое моделирование/1. Февраль/06.02.2026/individual.py
@@ -1,4 +1,4 @@
-from monte_carlo import (
+from montecarlo import (
 	MonteCarlo, 
 	os, 
 	np
@@ -8,29 +8,29 @@ from monte_carlo import (
 
 if __name__ == "__main__":
 	data = [
-		{"a": 0, "b": 1, "N": 10 ** 2, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
-		{"a": 0, "b": 1, "N": 10 ** 3, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
-		{"a": 0, "b": 1, "N": 10 ** 4, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
-		{"a": 0, "b": 1, "N": 10 ** 5, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
-		{"a": 0, "b": np.pi / 2, "N": 10 ** 2, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
-		{"a": 0, "b": np.pi / 2, "N": 10 ** 3, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
-		{"a": 0, "b": np.pi / 2, "N": 10 ** 4, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
-		{"a": 0, "b": np.pi / 2, "N": 10 ** 5, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
-		{"a": -1, "b": 1, "N": 10 ** 2, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
-		{"a": -1, "b": 1, "N": 10 ** 3, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
-		{"a": -1, "b": 1, "N": 10 ** 4, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
-		{"a": -1, "b": 1, "N": 10 ** 5, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
-		{"a": 0, "b": 1, "N": 10 ** 2, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
-		{"a": 0, "b": 1, "N": 10 ** 3, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
-		{"a": 0, "b": 1, "N": 10 ** 4, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
-		{"a": 0, "b": 1, "N": 10 ** 5, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
+		{"ab": (0, 1), "N": 10 ** 2, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
+		{"ab": (0, 1), "N": 10 ** 3, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
+		{"ab": (0, 1), "N": 10 ** 4, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
+		{"ab": (0, 1), "N": 10 ** 5, "f": lambda x: x ** .5, "f_text": "x^(1/2)", "manual_result": 2/3},
+		{"ab": (0, np.pi / 2), "N": 10 ** 2, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
+		{"ab": (0, np.pi / 2), "N": 10 ** 3, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
+		{"ab": (0, np.pi / 2), "N": 10 ** 4, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
+		{"ab": (0, np.pi / 2), "N": 10 ** 5, "f": lambda x: np.sin(x) ** 2, "f_text": "sin^2", "manual_result": np.pi/4},
+		{"ab": (-1, 1), "N": 10 ** 2, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
+		{"ab": (-1, 1), "N": 10 ** 3, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
+		{"ab": (-1, 1), "N": 10 ** 4, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
+		{"ab": (-1, 1), "N": 10 ** 5, "f": lambda x: x ** 3 + 1, "f_text": "x^3 + 1", "manual_result": 2},
+		{"ab" : (0, 1),"N": 10 ** 2, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
+		{"ab" : (0, 1),"N": 10 ** 3, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
+		{"ab" : (0, 1),"N": 10 ** 4, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
+		{"ab" : (0, 1),"N": 10 ** 5, "f": lambda x: 1 / (1 + x), "f_text": "1 / 1 + x", "manual_result": np.log(2)},
 	]
 
 	for parametrs in data:
 		obj = MonteCarlo(**parametrs)
 		result = obj.estimate
 		print(
-			f"Интервал: [{parametrs["a"]}, {parametrs["b"]}] -> [{obj.a}, {obj.b}]",
+			f"Интервал: {parametrs["ab"]}",
 			f"N = {obj.N}",
 			f"Метод решения: {obj.method}",
 			f"Функция: {parametrs["f_text"]}",
@@ -39,5 +39,5 @@ if __name__ == "__main__":
 			f"Погрешность: {abs(result - parametrs["manual_result"])} (+-{result / parametrs["manual_result"]} %)",
 			"-" * 70,
 			sep="\n",
-			file=open(f"{os.path.dirname(__file__)}/individual.txt", "a", encoding="utf-8")
+			file=open(f"{os.path.dirname(__file__)}/results/individual.txt", "a", encoding="utf-8")
 		)
